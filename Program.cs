944c9e4b68b3367304d04ec80d58a4280d3b567b
GilShalit <34002589+GilShalit@users.noreply.github.com>
2022-01-06 21:27:58 +0200

    code causing error

--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
 using AutoComplete_repo;
+using Blazorise;
+using Blazorise.Bootstrap;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services
+      .AddBlazorise(options =>
+      {
+          options.ChangeTextOnKeyPress = true;
+      })
+      .AddBootstrapProviders();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
